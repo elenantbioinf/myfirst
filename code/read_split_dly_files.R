62dd4b3fe39747572bb888063a564b96ccfa4dc3
Elena Nuñez <elena@Elena.localdomain>
2025-10-30 23:25:01 +0100

    Merge wheather station data with precipitation data

--- code/read_split_dly_files.R
+++ code/read_split_dly_files.R
@@ -79,21 +79,22 @@ window <- 30
 
 process_xfiles <- function(x){
   print(x)
+  
   read_fwf(x,
          fwf_widths(widths, headers),
          na = c("NA", "-9999"),
          col_types = cols(.default = col_character()),
          col_select = c(ID, YEAR, MONTH, starts_with("VALUE"))) %>% 
-  rename_all(tolower) %>%
-  pivot_longer(cols = starts_with("value"),
+    rename_all(tolower) %>%
+    pivot_longer(cols = starts_with("value"),
                names_to = "day", values_to = "prcp") %>%
-  drop_na() %>%
-  filter(prcp != 0) %>%
-  mutate(day = str_replace(day, "value", ""),
-         date = ymd(glue("{year}-{month}-{day}")),
+    mutate(day = str_replace(day, "value", ""),
+         date = ymd(glue("{year}-{month}-{day}"),  quiet = TRUE),
+         prcp = replace_na(prcp, "0"),
          prcp = as.numeric(prcp)/100) %>%
-  select(id, date, prcp) %>%
-  mutate(julian_day = yday(date),
+    drop_na(date) %>%
+    select(id, date, prcp) %>%
+    mutate(julian_day = yday(date),
          diff = tday_julian - julian_day,
          is_in_window = case_when(
            diff < window & diff > 0 ~ TRUE,
@@ -102,9 +103,9 @@ process_xfiles <- function(x){
            diff < 0 ~ FALSE),
          year = year(date),
          year = if_else(diff < 0 & is_in_window, year + 1, year)) %>%
-  filter(is_in_window) %>%
-  group_by(id, year) %>%
-  summarize(prcp = sum(prcp), .groups = "drop")
+    filter(is_in_window) %>%
+    group_by(id, year) %>%
+    summarize(prcp = sum(prcp), .groups = "drop")
 }
 
 
